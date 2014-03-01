2e58e112e5fb0139bdc82ff256a1e2c6219ac066
Steve Theoodre <steve@theodox.com>
2014-03-01 10:57:00 -0800

    Cleaning up nomenclature, re-adding Bindable setup
    Fixing a mix up in the branches
    
    Format fixes only in core.py

--- mGui/mGui/bindings.py
+++ mGui/mGui/bindings.py
@@ -7,16 +7,15 @@ import maya.cmds as cmds
 from collections import Mapping
 import weakref
 
-BINDABLE_VALUES = ['value', 'label']
 
-BREAK_ON_BIND_FAILURE = False
+BREAK_ON_BIND_FAILURE = True
 
 
 class BindingError(ValueError):
     pass
 
 
-class Binding(object):
+class Accessor(object):
     '''
     Bind to an object field.
     '''
@@ -30,16 +29,33 @@ class Binding(object):
     def _get(self, *args, **kwargs):
         return getattr(self.Target, self.FieldName)
 
-    def update(self, *args, **kwargs):
+    def push(self, *args, **kwargs):
+        '''
+        Set the value in <args> on <Target.FieldName>
+        '''
         try:
             self._set(*args, **kwargs)
         except:
             if BREAK_ON_BIND_FAILURE:
                 raise
 
+    def pull(self, *args, **kwargs):
+        '''
+        Returns the value from <Target.Fieldname>
+        '''
+        try:
+            return self._get(*args, **kwargs)
+        except:
+            if BREAK_ON_BIND_FAILURE:
+                raise
+            else:
+                return ''
 
-class DictBinding(Binding):
-    '''Bind to a dictionary entry'''
+
+class DictAccessor(Accessor):
+    '''
+    Accessor for a dictionary entry
+    '''
     def _set(self, *args, **kwargs):
         self.Target[self.FieldName] = args[0]
 
@@ -47,8 +63,10 @@ class DictBinding(Binding):
         return self.Target[self.FieldName]
 
 
-class PyNodeBinding(Binding):
-    '''Bind to an attribute on a PyNode'''
+class PyNodeAccessor(Accessor):
+    '''
+    Accessor fpr  an attribute on a PyNode
+    '''
     def _set(self, *args, **kwargs):
         getattr(self.Target, self.FieldName).set(args[0])
 
@@ -56,10 +74,12 @@ class PyNodeBinding(Binding):
         return self.Target.attr(self.FieldName).get()
 
 
-class CmdsBinding(Binding):
-    '''Bind to a maya attribute string'''
+class CmdsAccessor(Accessor):
+    '''
+    Accessor for a maya attribute string
+    '''
     def __init__(self, datum, fieldName):
-        super(CmdsBinding, self).__init__(datum, fieldName)
+        super(CmdsAccessor, self).__init__(datum, fieldName)
         self._attrib = self.Target + "." + self.FieldName
 
     def _set(self, *args, **kwargs):
@@ -69,31 +89,195 @@ class CmdsBinding(Binding):
         return cmds.getAttr(self._attrib)
 
 
-class MethodBinding(Binding):
+class MethodAccessor(Accessor):
     '''
-    Bind to a method
+    Accessor for a method
     '''
     def _set(self, *args, **kwargs):
-        getattr(self.Target, self.FieldName)(args[0])
+        getattr(self.Target, self.FieldName)(*args, **kwargs)
+
+    def _get(self, *args, **kwargs):
+        return getattr(self.Target, self.FieldName)(*args, **kwargs)
 
 
-def binding(target, targetField):
+def get_accessor(target, targetField):
     '''
-    Returns an appropriate Binding object for the supplied target and target
+    Returns an appropriate Accessor object for the supplied target and target
     field.
     '''
     if isinstance(target, Mapping):
-        return DictBinding(target, targetField)
+        return DictAccessor(target, targetField)
     if hasattr(target, '__melcmd__'):
-        return PyNodeBinding(weakref.proxy(target), targetField)
+        return PyNodeAccessor(weakref.proxy(target), targetField)
     if hasattr(target, targetField):
         if callable(getattr(target, targetField)):
-            return MethodBinding(weakref.proxy(target), targetField)
+            return MethodAccessor(weakref.proxy(target), targetField)
         else:
-            return Binding(weakref.proxy(target), targetField)
+            return Accessor(weakref.proxy(target), targetField)
     try:
         cmds.attributeQuery(targetField, node=target, w=True)
-        return CmdsBinding(target, targetField)
+        return CmdsAccessor(target, targetField)
     except RuntimeError:
-        raise BindingError ("%s is not a bindable attribute of %s" % (targetField, target))
+        tgt_string = '%s.%s' % (targetField, target)
+        raise BindingError("%s is not a bindable attribute of %s" % tgt_string)
+
+
+class BindingContext(object):
+
+    ACTIVE = None
+
+    def __init__(self, update_on_close=True):
+        self.Bindings = []
+        self._cache_context = None
+        self._update_on_close = update_on_close
+
+    def __enter__(self):
+        self._cache_context = self.ACTIVE
+        BindingContext.ACTIVE = self
+        return self
+
+    def __exit__(self, typ, value, traceback):
+        BindingContext.ACTIVE = self._cache_context
+        if self._update_on_close:
+            self.update()
+
+    def update(self):
+        '''
+        update all bindings in thie context
+        '''
+        delenda = [i for i in self.Bindings if not i()]
+        for item in delenda:
+            self.Bindings.remove(item)
+        return len(self.Bindings)
+
+    @classmethod
+    def add(cls, binding):
+        print cls, binding, cls.ACTIVE.Bindings
+        if cls.ACTIVE:
+            cls.ACTIVE.Bindings.append(binding)
+
+
+class Binding(object):
+    '''
+    Encapsulates a data binding (get accessor and  a set accessor)
+    '''
+
+    def __init__(self, source, target, *extra, **kwargs):
+        try:
+            if extra:
+                src = (source, target)
+                tgt = (extra[0:2])
+            else:
+                src = source
+                tgt = target
+
+            self.Getter = get_accessor(*src)
+            self.Setter = get_accessor(*tgt)
+
+        except IndexError:
+            raise BindingError('get_accessor requires 2 or 4 arguments')
+
+        self.Translator = kwargs.get('translate', lambda v: v)
+        assert callable(self.Translator), 'Translator must be a single argument callable'
+
+        BindingContext.add(self)
+
+    def __call__(self):
+        try:
+            self.Setter.push(self.Translator(self.Getter.pull()))
+            return True
+        except ReferenceError:
+            return False
+
+
+class Bindable (object):
+
+    def site(self):
+        return self
+
+    def __rshift__(self, other):
+        if not self.bind_source:
+            raise BindingError("bind source is not set for %s" % self)
+        if hasattr(other, 'bind_target'):
+            return Binding(self.site(), self.bind_source, other.site(), other.bind_target)
+        elif len(other) == 2:
+            return Binding((self.site(), self.bind_source), other)
+        raise BindingError("Invalid bind target %s" % str(other))
+
+    def __lshift__(self, other):
+        if not self.bind_target:
+            raise BindingError("bind target is not set for %s" % self)
+        if hasattr(other, 'bind_source'):
+            return Binding(other.site(), other.bind_source, self.site(), self.bind_target)
+        elif len(other) == 2:
+            return Binding(other, (self.site(), self.bind_target))
+        raise BindingError("Invalid bind target %s" % str(other))
+
+    def __floordiv__(self, other):
+        return BindProxy(self, other)
+
+
+class BindableObject(Bindable):
+    _BIND_SRC = None
+    _BIND_TGT = None
+
+    def __new__(self, *args, **kwargs):
+        obj = object.__new__(self)
+        self.bind_source = self._BIND_SRC
+        self.bind_target = self._BIND_TGT
+        return obj
+
+
+class BindProxy(Bindable):
+    def __init__(self, item, attrib):
+        self.Item = item
+        self.bind_source = self.bind_target = attrib
+
+    def site(self):
+        return self.Item
+
+
+class Example(BindableObject):
+    _BIND_SRC = 'Name'
+    _BIND_TGT = 'Val'
+
+    def __init__(self, name, val):
+        self.Name = name
+        self.Val = val
+
+    def __str__(self):
+        return 'Name %s Val %s' % (self.Name, self.Val)
+
+
+
+import types
+
+def test():
+
+    with BindingContext() as ggg:
+        dino = Example("dino", 99)
+        dino.food = "bone"
+        barney = Example("barney", "rubble")
+        barney.hair = "yellow"
+        fred = Example("fred", "flintstone")
+        def test(self, val): self.Name = val.upper()
+        def zzz (self): return self.Name + "_" + self.Val
+        fred.up = types.MethodType(test, fred)
+        fred.down = types.MethodType(zzz, fred)
+        barney << fred
+        barney // 'hair' >> dino
+        wilma = {'wilma':'wilma'}
+        wilma['wilma'] = 'nope'
+        barney // 'Val' << (wilma, 'wilma')
+        q = Binding(fred, 'down', wilma, 'down', translate=str.upper)
+
+    # #ggg.update()
+    print ggg.Bindings
+
+    print fred
+    print barney
+    print dino
+    print wilma
+
+test()
 
--- mGui/mGui/core.py
+++ mGui/mGui/core.py
@@ -6,22 +6,22 @@ class CtlProperty (object):
     '''
     Property descriptor.  When applied to a Control-derived class, invokes the correct Maya command under the hood to get or set values
     '''
-    
-    def __init__(self, flag, cmd, writeable = True):
+
+    def __init__(self, flag, cmd, writeable=True):
         assert callable(cmd), "cmd flag must be a maya command for editing gui objects"
         self.Flag = flag or self.FLAG
         self.Writeable = writeable
-        self.Command = cmd 
+        self.Command = cmd
 
     def __get__(self, obj, objtype):
         return self.Command(obj.Widget, **{'q':True, self.Flag:True})
-    
+
     def __set__(self, obj, value):
         if not self.Writeable:
             raise AttributeError('attribute .%s is not writable' % self.Flag)
         return self.Command(obj.Widget, **{'e':True, self.Flag:value})
-        
-                
+
+
 class CallbackProperty(object):
     '''
     Property descriptor for callbacks.  When accessed, returns the appropriate Event object from a Control-derived class's Callback dictionary. This property cannot be 'set' or 'unset' - instead use the += or -= methods on the underlying Event object:
@@ -35,7 +35,7 @@ class CallbackProperty(object):
     '''
     def __init__(self, key):
         self.Key = key
-        
+
     def __get__(self, obj, objtype):
         cb = obj.Callbacks.get(self.Key, None)
         if not cb:
@@ -51,18 +51,18 @@ class ControlMeta(type):
     '''
 
     def __new__(cls, name, parents, kwargs):
-        
+
 
         CMD = kwargs.get('CMD', None)
-        _READ_ONLY = kwargs.get('_READ_ONLY',[])
-        _ATTRIBS = kwargs.get('_ATTRIBS',[])
-        _CALLBACKS = kwargs.get('_CALLBACKS',[])
-        
+        _READ_ONLY = kwargs.get('_READ_ONLY', [])
+        _ATTRIBS = kwargs.get('_ATTRIBS', [])
+        _CALLBACKS = kwargs.get('_CALLBACKS', [])
+
         if not kwargs.get('CMD'):
             CMD = parents[0].CMD
-            
+
         for item in _READ_ONLY:
-            kwargs[item] = CtlProperty(item, CMD, writeable = False)           
+            kwargs[item] = CtlProperty(item, CMD, writeable=False)
         for item in _ATTRIBS:
             kwargs[item] = CtlProperty(item, CMD)
         for item in _CALLBACKS:
@@ -75,20 +75,20 @@ class Control(object):
     Base class for all mGui controls.  Provides the necessary frameworks for CtlProperty and CallbackProperty access to the underlying widget
     '''
     CMD = cmds.control
-    _ATTRIBS = ['annotation', 'backgroundColor', 'defineTemplate', 'docTag', 'dragCallback', 'dropCallback', 'enable', 'enableBackground', 'exists', 'fullPathName', 'height',  'manage', 'noBackground', 'numberOfPopupMenus', 'parent', 'popupMenuArray', 'preventOverride', 'useTemplate', 'visible', 'visibleChangeCommand', 'width']
+    _ATTRIBS = ['annotation', 'backgroundColor', 'defineTemplate', 'docTag', 'dragCallback', 'dropCallback', 'enable', 'enableBackground', 'exists', 'fullPathName', 'height', 'manage', 'noBackground', 'numberOfPopupMenus', 'parent', 'popupMenuArray', 'preventOverride', 'useTemplate', 'visible', 'visibleChangeCommand', 'width']
     _CALLBACKS = ['dragCallback', 'dropCallback', 'visibleChangeCommand']
     _READ_ONLY = ['isObscured', 'popupMenuArray', 'numberOfPopupMenus']
     __metaclass__ = ControlMeta
-    
-    
-    def __init__(self, name,  *args, **kwargs):
+
+
+    def __init__(self, name, *args, **kwargs):
 
         self.Name = name
-        
+
         self.Style = kwargs.get('style', self.__class__.__name__)
         if 'style' in kwargs: del kwargs['style']
-   
-        
+
+
         self.Widget = self.CMD(*args, **kwargs)
         '''
         Widget is the gui element in the scene
@@ -99,31 +99,31 @@ class Control(object):
         '''
 
         Layout.add_current(self)
-        
+
     def register_callback(self, callbackName, event):
         '''
         when a callback property is first accessed this creates an Event for the specified callback and hooks it to the gui widget's callback function
         '''
         kwargs = {'e':True, callbackName:event}
         self.CMD(self.Widget, **kwargs)
-                
-    
-                
+
+
+
     def __nonzero__(self):
         return self.exists
-    
+
     def __repr__(self):
         if self:
             return self.Widget
         else:
             return "<deleted UI element %s>" % self.__class__
-        
+
     def __str__(self):
         return self.Widget
-    
+
     def __iter__(self):
         yield self
-        
+
 
 
 
@@ -132,49 +132,48 @@ class Control(object):
 # instead of cmds.control. In Maya 2014, using cmds.control to query a layout fails, evem for flags they have in common
 
 class Layout(Control):
-    
+
     CMD = cmds.layout
-    _ATTRIBS = ['annotation', 'backgroundColor', 'defineTemplate', 'docTag', 'dragCallback', 'dropCallback', 'enable', 'enableBackground', 'exists', 'fullPathName', 'height',  'manage', 'noBackground', 'numberOfPopupMenus', 'parent', 'popupMenuArray', 'preventOverride', 'useTemplate', 'visible', 'visibleChangeCommand', 'width']
+    _ATTRIBS = ['annotation', 'backgroundColor', 'defineTemplate', 'docTag', 'dragCallback', 'dropCallback', 'enable', 'enableBackground', 'exists', 'fullPathName', 'height', 'manage', 'noBackground', 'numberOfPopupMenus', 'parent', 'popupMenuArray', 'preventOverride', 'useTemplate', 'visible', 'visibleChangeCommand', 'width']
     _CALLBACKS = ['dragCallback', 'dropCallback', 'visibleChangeCommand']
     _READ_ONLY = ['isObscured', 'popupMenuArray', 'numberOfPopupMenus', 'childArray', 'numberOfChildren']
     ACTIVE_LAYOUT = None
-    
-    def __init__(self, name,   *args, **kwargs):
+
+    def __init__(self, name, *args, **kwargs):
         super (Layout, self).__init__(self, name, *args, **kwargs)
         self.Controls = []
         self.__cache_layout = self.ACTIVE_LAYOUT
         Layout.ACTIVE_LAYOUT = self
-        
-    def __enter__( self ):
+
+    def __enter__(self):
         return self
 
-    def __exit__( self, typ, value, traceback ):
-        cmds.setParent( ".." ) #@UndefinedVariable
+    def __exit__(self, typ, value, traceback):
+        cmds.setParent("..")  # @UndefinedVariable
         self.ACTIVE_LAYOUT = self.__cache_layout
         self.layout()
-        
+
     def layout(self):
         pass
-        
+
     def add(self, control):
         if control.Name in self.__dict__:
             raise RuntimeError, 'Children of a layout must have unique IDs'
         self.Controls.append(control)
         self.__dict__[control.Name] = control
-        
+
     def remove(self, control):
         self.Controls.remove(control)
         k = [k for k , v in self.__dict__.items() if v == control]
-        if k: del[self.__dict__ [k[0]]]       
-    
+        if k: del[self.__dict__ [k[0]]]
+
     def __iter__(self):
         for item in self.Controls:
             for sub in item:
                 yield sub
         yield self
-    
+
     @classmethod
     def add_current(cls, control):
         if control.Name and cls.ACTIVE_LAYOUT:
             cls.ACTIVE_LAYOUT.add(control)
-    
