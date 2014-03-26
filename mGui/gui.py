59486de371809cf57d03461bdc5a1fd4bdf88501
Steve Theodre <spam@theodox.com>
2014-03-25 23:28:32 -0700

    docs

--- mGui/gui.py
+++ mGui/gui.py
@@ -1,3 +1,10 @@
+'''
+mGui.gui
+
+forwards all of the widget definitions in the system for easy import.  This
+module is probably safe to import * in a known context
+'''
+
 from mGui.core import Window
 from mGui.controls import *
 from mGui.layouts import *
