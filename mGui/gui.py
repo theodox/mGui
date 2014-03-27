00ba81f34548c49bfede9d1a72746334a71dde01
Steve Theodre <spam@theodox.com>
2014-03-26 22:59:15 -0700

    updated to reflect core package organization

--- mGui/gui.py
+++ mGui/gui.py
@@ -5,9 +5,9 @@ forwards all of the widget definitions in the system for easy import.  This
 module is probably safe to import * in a known context
 '''
 
-from mGui.core import Window
-from mGui.controls import *
-from mGui.layouts import *
-from mGui.menus import Menu, MenuItem
-from mGui.forms import *
 
+from mGui.core import Window, BindingWindow 
+from mGui.core.controls import *
+from mGui.core.layouts import *
+from mGui.core.menus import Menu, MenuItem
+from mGui.forms import *
