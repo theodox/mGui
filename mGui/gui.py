62f2f7817fe391cab27895399fc30084d9e4a157
theodox <steve@theodox.com>
2014-04-09 13:13:51 -0700

    added OptionMenu and PopupMenu support

--- mGui/gui.py
+++ mGui/gui.py
@@ -6,8 +6,8 @@ module is probably safe to import * in a known context
 '''
 
 
-from mGui.core import Window, BindingWindow 
+from mGui.core import Window, BindingWindow
 from mGui.core.controls import *
 from mGui.core.layouts import *
-from mGui.core.menus import Menu, MenuItem
+from mGui.core.menus import *
 from mGui.forms import *
