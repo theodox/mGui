d46e28176cf861de9eeb5cb5bea912d009b06282
Steve Theodre <spam@theodox.com>
2014-04-09 23:00:19 -0700

    better docs

--- mGui/examples/simple.py
+++ mGui/examples/simple.py
@@ -5,6 +5,31 @@ from mGui.bindings import BindProxy, BindingContext, bind, Bindable
 class ExampleWindow(Bindable):
     '''
     This example simple window with color sliders
+    
+    This showcases two different methods of passing information between parts of the UI.
+    
+    The sliders and buttons use more or less conventional events to update the
+    instance field "Color", which is an RGB value.  This is pretty much the way
+    its always done in Maya, although this example uses the += event binding
+    syntax to attach the event handlers rather than the vanilla command = handler 
+    syntax.
+    
+    The color and the text display, however, are driven by bindings; the buttons and sliders
+    update the Color field, and the bindings make sure that the swatch and overlay text
+    display the current values. Since the window is a BindingWindow, it comes with a 
+    built-in BindingContext (see bindings.py for details) so all the text and color
+    bindings are updated with a single call.
+    
+    The slider handlers can all share a function through the use of the Tag field.
+    
+    Also of interest, the CSS style at the top sets the style context for all of the 
+    FloatSliderButtonGrp widgets so they are lined up neatly.  Notice that inherits from
+    stylesheets.defaults(), which includes a base style for all '_Grp' controls to set the 
+    text aligments and sizes of the labels consistently.
+    
+    Note on Maya 2011 this color swacth does not fill the entire right-hand
+    panel; on Maya 2014 it does. This appears to be a change in the behavior of
+    the underlying widget between versions.
     '''    
 
     
@@ -21,7 +46,7 @@ class ExampleWindow(Bindable):
             with gui.BindingWindow('window', title = 'simple example',  width=512) as self.Window:
                 with gui.HorizontalStretchForm('main', width = 512):
                     with gui.VerticalForm('controls', width = 256) as sliders:
-                        self.xXX= gui.FloatSliderButtonGrp('red', label = 'red', tag = 0)
+                        gui.FloatSliderButtonGrp('red', label = 'red', tag = 0)
                         gui.FloatSliderButtonGrp('green', label = 'green', tag = 1)
                         gui.FloatSliderButtonGrp('blue', label = 'blue', tag = 2)
                     with gui.FillForm('swatch', width = 256):
