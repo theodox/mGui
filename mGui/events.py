fb30ca51bcc0e87c3940e6ae2fbf21f3cca570e5
theodox <steve@theodox.com>
2014-05-02 15:27:48 -0700

    better detection of objects which have not gone out of scope
    The previous version used if self.c(): to check weakRef integrity; this
    failed for objects with an oddball definition of __nonzero__ -
    particularly instances of Control!  So, this version explicitly checks
    for False or None.

--- mGui/events.py
+++ mGui/events.py
@@ -10,13 +10,14 @@ import weakref
 import maya.utils
 from functools import  partial
 
-class Event( object ):
+
+class Event(object):
     '''
     Simple event handler, similar to the standard c# event pattern. The object
     raising an event calls this event object as a callable; the object will in
     turn fire any of the callables it stores in its Handlers list, passing the
     args and kwargs provided by the original caller.
-    
+
     sample usage:
         test = Event()
 
@@ -26,11 +27,11 @@ class Event( object ):
         > test( 'arg1', 'arg2', e="fred" )
         A ('arg1', 'arg2') {'e': 'fred', 'event': <Event object at 0x00000000026892E8>}
 
-    the handlers are stored as weakrefs, so they will not keep their referents alive if those referents exists 
+    the handlers are stored as weakrefs, so they will not keep their referents alive if those referents exists
     in no other scope. For example:
 
         > x = Event()
-        > def test(*args, **kwargs): 
+        > def test(*args, **kwargs):
         >     print "hello world"
         > x += test
         > x()
@@ -39,52 +40,52 @@ class Event( object ):
         > x()
 
     Handlers must exhibit the *args, **kwargs signature.  It's the handler's job
-    to decide what to do with them but they will be passed.  
-    
+    to decide what to do with them but they will be passed.
+
     Events can be given 'metadata' - arguments that are passed in at creation time:
-    
+
         x = Event(name = 'test_event')
         def test (*args, *kwargs):
             print args, kwargs
-            
+
         x()
         {'name': 'test_event', 'event': <Event object at 0x00000000026892E8>}
-        
+
     Metadata added when the Event is first created will be included in every
     firing of the event. Arguments and keywords can also be associated with a
     particular firing:
-    
+
         x = Event(name = 'test_event')
         def test (*args, *kwargs):
-            print "args:" , args
+            print "args:", args
             print "kwargs:", kwargs
-            
+
         x('hello')
         args: hello
         kwargs: {'name': 'test_event', 'event': <Event object at 0x00000000026892E8>}
-    
+
         x('world')
         args: world
         kwargs: {'name': 'test_event', 'event': <Event object at 0x00000000026892E8>}
-    
+
     '''
 
-    def __init__( self, **data):
+    def __init__(self, **data):
         self._Handlers = set()
         '''Set list of handlers callables. Use a set to avoid multiple calls on one handler'''
         self.Data = data
         self.Data['event'] = self
-        
-    def _addHandler( self, handler ):
+
+    def _addHandler(self, handler):
         '''
         Add a handler callable. Raises a ValueError if the argument is not callable
         '''
         wr = WeakMethod(handler)
-       
-        self._Handlers.add( wr )
+
+        self._Handlers.add(wr)
         return self
 
-    def _removeHandler( self, handler ):
+    def _removeHandler(self, handler):
         '''
         Remove a handler. Ignores handlers that are not present.
         '''
@@ -92,7 +93,7 @@ class Event( object ):
         delenda = [h for h in self._Handlers if h == wr]
         self._Handlers = self._Handlers.difference(set(delenda))
         return self
-    
+
     def metadata(self, kwargs):
         '''
         returns the me
@@ -102,24 +103,24 @@ class Event( object ):
         md.update(kwargs)
         return md
 
-    def _fire( self, *args, **kwargs ):
+    def _fire(self, *args, **kwargs):
         '''
         Call all handlers.  Any decayed references will be purged.
         '''
-        
+
         delenda = []
         for handler in self._Handlers:
             try:
-                handler( *args, **self.metadata(kwargs) )
+                handler(*args, **self.metadata(kwargs))
             except DeadReferenceError:
                 delenda.append(handler)
         self._Handlers = self._Handlers.difference(set(delenda))
 
-    def _handlerCount ( self ):
+    def _handlerCount (self):
         '''
         Returns the count of the _Handlers field
         '''
-        return len( [i for i in self._Handlers] )
+        return len([i for i in self._Handlers])
 
     # hook up the instance methods to the base methods
     # doing it this way allows you to override more neatly
@@ -134,15 +135,15 @@ class MayaEvent(Event):
     '''
     Subclass of event that uses Maya.utils.executeDeferred.
     '''
-    def _fire( self, *args, **kwargs ):
+    def _fire(self, *args, **kwargs):
         '''
         Call all handlers.  Any decayed references will be purged.
         '''
-               
+
         delenda = []
-        for handler in self._Handlers:        
+        for handler in self._Handlers:
             try:
-                maya.utils.executeDeferred( partial(handler,  *args, **self.metadata(kwargs) ) )
+                maya.utils.executeDeferred(partial(handler, *args, **self.metadata(kwargs)))
             except DeadReferenceError:
                 delenda.append(handler)
         self._Handlers = self._Handlers.difference(set(delenda))
@@ -158,57 +159,61 @@ class DeadReferenceError(TypeError):
     '''
     pass
 
-## create weak references to both bound and unbound methods
-## hat tip to  Frederic Jolliton on ActiveState 
+# # create weak references to both bound and unbound methods
+# # hat tip to  Frederic Jolliton on ActiveState
 
-class WeakMethodBound :
+
+class WeakMethodBound:
     '''
     Encapsulates a weak reference to a bound method on an object.  Has a
-    hashable ID so that Events can identify multiple references to the saame
+    hashable ID so that Events can identify multiple references to the same
     method and not duplicate them
     '''
-    def __init__( self , f ) :
-        
+    def __init__(self, f):
+
         self.f = f.im_func
-        self.c = weakref.ref( f.im_self )
-        self.ID = id(f.im_self) ^  id(f.im_func.__name__)
-        
-    def __call__( self , *arg, **kwarg ) :
-        if self.c():
-            return apply( self.f , ( self.c() , ) + arg, kwarg )
+        self.c = weakref.ref(f.im_self)
+        self.ID = id(f.im_self) ^ id(f.im_func.__name__)
+
+    def __call__(self, *arg, **kwarg):
+        ref = self.c()
+        if not ref is False and not ref is None:
+            return apply(self.f, (self.c(),) + arg, kwarg)
         else:
             raise DeadReferenceError
 
     def __eq__(self, other):
-        if not hasattr(other, 'ID'): return False
+        if not hasattr(other, 'ID'):
+            return False
         return self.ID == other.ID
-    
+
     def __hash__(self):
         return self.ID
-        
-class WeakMethodFree :
+
+
+class WeakMethodFree:
     '''
     Encapsulates a weak reference to an unbound method
     '''
-    def __init__( self , f ) :
-        self.f = weakref.ref( f )
-        self.ID = id (f)
-        
-    def __call__( self , *arg , **kwarg) :
+    def __init__(self, f):
+        self.f = weakref.ref(f)
+        self.ID = id(f)
+
+    def __call__(self, *arg, **kwarg) :
         if self.f():
-            return apply( self.f() , arg , kwarg)
+            return apply(self.f(), arg, kwarg)
         else:
             raise DeadReferenceError
-    
+
     def __eq__(self, other):
         if not hasattr(other, 'ID'): return False
         return self.ID == other.ID
-    
+
     def __hash__(self):
         return self.ID
-    
 
-def WeakMethod( f ) :
+
+def WeakMethod(f) :
     '''
     Returns a WeakMethodFree or a WeakMethodBound for the supplied function, as
     appropriate
@@ -216,5 +221,5 @@ def WeakMethod( f ) :
     try :
         f.im_func
     except AttributeError :
-        return WeakMethodFree( f )
-    return WeakMethodBound( f )
+        return WeakMethodFree(f)
+    return WeakMethodBound(f)
