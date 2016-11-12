0e236ccd862f816101deb9792f88ced2d61fbc9b
bob-white <robert.wd.white@gmail.com>
2016-11-12 10:02:16 -0600

    examples/simpleBinding.py update
    Updated to use the new keyless syntax.
    Moved the bindings operations to separate lines, mostly to make them a bit more visible.

--- mGui/examples/simpleBinding.py
+++ mGui/examples/simpleBinding.py
@@ -3,35 +3,39 @@ Warning: If you give this layout less than one child there will be an error.
 Example:
 import mGui.examples.simpleBinding as simpleBinding
 reload(simpleBinding)
-example = simpleBinding.window_object()
+example = simpleBinding.ExampleWindow()
 example.settings
 """
 
 import maya.cmds as cmds
-import mGui.gui as gui
-import mGui.forms as forms
+from mGui import gui, forms
 from mGui.bindings import bind
 
 
-class window_object(object):
+class ExampleWindow(object):
     settings = {'namespace': 'Starting example, type here'}
 
     def __init__(self):
-        with gui.Window( title="Example") as window:
-            with forms.VerticalForm(None):
-                self.thing1 = testItem(self.settings)
-                self.thing1.field.proxy_update()
-                gui.Button('poopy', label='print settings').command += self.query_settings
+        with gui.Window(title="Example") as self.window:
+            with forms.VerticalForm() as vf:
+                self.thing = ExampleItem(self.settings)
+                self.thing.field.update_bindings()
+                poopy = gui.Button(label='print settings')
+                poopy.command += self.query_settings
 
-        cmds.showWindow(window)
+        self.window.show()
 
     def query_settings(self, *args, **kwargs):
-        self.thing1.field.proxy_update()
+        self.thing.field.update_bindings()
         print 'self.settings =', self.settings
 
 
-class testItem(object):
+class ExampleItem(object):
 
     def __init__(self, settings):
         self.settings = settings
-        self.field = gui.TextField('namespace').bind.text | bind(lambda p: p or self.settings['namespace']) | (self.settings, 'namespace')
+        self.field = gui.TextField('namespace')
+        self.field.bind.text | bind(lambda p: p or self.settings['namespace']) | (self.settings, 'namespace')
+
+if __name__ == '__main__':
+    win = ExampleWindow()
