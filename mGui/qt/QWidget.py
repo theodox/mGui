7834f872ea369d9bf980bcf4bbbff49ae6b280fe
bob-white <robert.wd.white@gmail.com>
2017-01-09 18:41:30 -0600

    Updated the ModalWindow code a bit, fixed an issue with the signature on forget, and added dimiss as a more dialog appropriate alternative. Major difference is dismiss will delete the window when called.

--- mGui/qt/QWidget.py
+++ mGui/qt/QWidget.py
@@ -1,15 +1,16 @@
 
 from ..core import BindingWindow
 
+from maya import cmds
 from maya.OpenMayaUI import MQtUtil
 
 try:
     from shiboken import wrapInstance
-    from PySide.QtGui import QWidget
+    from PySide.QtGui import QWidget, QDialog
     from PySide import QtCore
 except ImportError:
     from shiboken2 import wrapInstance
-    from PySide2.QtWidgets import QWidget
+    from PySide2.QtWidgets import QWidget, QDialog
     from PySide2 import QtCore
 
 class ModalWindow(BindingWindow):
@@ -18,14 +19,23 @@ class ModalWindow(BindingWindow):
         super(ModalWindow, self).__init__(*args, **kwargs)
         ptr = MQtUtil.findWindow(self.widget)
         self._qt_obj = wrapInstance(long(ptr), QWidget)
-        self._qt_obj.setWindowModality(QtCore.Qt.WindowModality.WindowModal)
+        self._qt_obj.setWindowModality(QtCore.Qt.WindowModality.ApplicationModal)
+        
 
     def show(self):
         self._qt_obj.show()
 
-    def forget(self):
+    def forget(self, *args, **kwargs):
         super(ModalWindow, self).forget()
         self._qt_obj = None
 
-    def hide(self, *args, **kwargs):
+    def hide(self):
         self._qt_obj.hide()
+
+    def dismiss(self, *args, **kwargs):
+        self.hide()
+        cmds.deleteUI(self)
+
+    def __exit__(self, typ, value, traceback):
+        mGui_expand_stack = True
+        super(ModalWindow, self).__exit__(typ, value, traceback)
