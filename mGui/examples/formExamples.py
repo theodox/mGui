303605bb5b59d30bc014090c81fbe7e0f62cc577
theodox <steve@theodox.com>
2017-01-13 21:10:09 -0800

    cosmetic upgrades and cleanup

--- mGui/examples/formExamples.py
+++ mGui/examples/formExamples.py
@@ -1,6 +1,10 @@
-import sys
 import maya.cmds as cmds
+
 from mGui import gui, forms
+
+"""
+This example creates a window which shows many of the different types of form layouts in mGui.
+"""
 
 
 def create_sphere(*args, **kwargs):
@@ -54,10 +58,15 @@ def example_Forms(*args, **kwargs):
                 ("VerticalStretchForm", example_VerticalStretchForm),
                 ("VerticalThreePane", example_VerticalThreePane)]
 
-    with gui.Window(title="Forms Examples") as window:
-        with forms.VerticalExpandForm(width=320) as main:
-            for example_name, example_command in examples:
-                gui.Button(label=example_name).command += example_command
+    with gui.Window(title="Forms Examples", height=128) as window:
+        with forms.FillForm(margin=(12, 12)):
+            with forms.HeaderForm(width=320) as main:
+                gui.ScrollField(height=80,
+                                text="""This example shows many of the different kinds of formlayout presets in mGui.forms. Click buttons to show examples, and resize the windows to see the behavior""",
+                                ww=True, ed=0)
+                with forms.VerticalForm(spacing=(0, 4)):
+                    for example_name, example_command in examples:
+                        gui.Button(label=example_name).command += example_command
 
     window.show()
 
@@ -69,9 +78,9 @@ def example_FillForm(*args, **kwargs):
     formExamples.example_FillForm()
     """
     with gui.Window(title="FillForm") as window:
-        with forms.FillForm(width=320) as main:
+        with forms.FillForm(width=320, margin=(12, 12)) as main:
             for label, command in COMMANDS:
-                gui.Button(label=label).command += command
+                gui.Button(label="Expands to fill form").command += command
 
     window.show()
 
@@ -84,12 +93,9 @@ def example_FooterForm(*args, **kwargs):
     formExamples.example_FooterForm()
     """
     with gui.Window(title="FooterForm") as window:
-        with forms.FooterForm(width=320) as main:
-            for index, (label, command) in enumerate(COMMANDS):
-                if index > 1:
-                    break
-                gui.Button(label=label).command += command
-
+        with forms.FooterForm(width=320, margin=(12, 12)) as main:
+            gui.ScrollField(text="expandable top section")
+            gui.Button(label='footer')
     window.show()
 
 
@@ -101,11 +107,9 @@ def example_HeaderForm(*args, **kwargs):
     formExamples.example_HeaderForm()
     """
     with gui.Window(title="HeaderForm") as window:
-        with forms.HeaderForm(width=320) as main:
-            for index, (label, command) in enumerate(COMMANDS):
-                if index > 1:
-                    break
-                gui.Button(label=label).command += command
+        with forms.HeaderForm(width=320, margin=(12, 12)) as main:
+            gui.Button(label='header')
+            gui.ScrollField(text="expandable bottom section")
 
     window.show()
 
@@ -117,9 +121,12 @@ def example_HorizontalExpandForm(*args, **kwargs):
     formExamples.example_HorizontalExpandForm()
     """
     with gui.Window(title="HorizontalExpandForm") as window:
-        with forms.HorizontalExpandForm(width=320) as main:
+        with forms.HorizontalExpandForm(margin=(12, 12)) as main:
+            gui.Button(label="Items Stack Horizontally")
+
             for label, command in COMMANDS:
                 gui.Button(label=label).command += command
+            gui.ScrollField(text="Last item expands", ed=0, ww=1)
 
     window.show()
 
@@ -130,10 +137,11 @@ def example_HorizontalForm(*args, **kwargs):
     import mGui.examples.formExamples as formExamples
     formExamples.example_HorizontalForm()
     """
-    with gui.Window(title="HorizontalForm") as window:
-        with forms.HorizontalForm(width=320) as main:
+    with gui.Window(title="HorizontalForm", height=64, width=512) as window:
+        with forms.HorizontalForm(width=320, margin=(12, 12)) as main:
+            gui.ScrollField(text="Components stack horizontally", ed=0, width=128, ww=True)
             for label, command in COMMANDS:
-                gui.Button(label=label).command += command
+                gui.Button(label=label, width=80).command += command
 
     window.show()
 
@@ -144,10 +152,12 @@ def example_HorizontalStretchForm(*args, **kwargs):
     import mGui.examples.formExamples as formExamples
     formExamples.example_HorizontalStretchForm()
     """
-    with gui.Window(title="HorizontalStretchForm", height=400) as window:
-        with forms.HorizontalStretchForm(width=320) as main:
+    with gui.Window(title="HorizontalStretchForm", height=64, width=512) as window:
+        with forms.HorizontalStretchForm(width=320, margin=(12, 12)) as main:
+            gui.ScrollField(text="Components stretch horizontally", ed=0, ww=True, width=64, height=40)
+
             for label, command in COMMANDS:
-                gui.Button(label=label).command += command
+                gui.Button(label=label, height=40).command += command
 
     window.show()
 
@@ -159,12 +169,15 @@ def example_HorizontalThreePane(*args, **kwargs):
     import mGui.examples.formExamples as formExamples
     formExamples.example_HorizontalThreePane()
     """
-    with gui.Window(title="HorizontalThreePane") as window:
-        with forms.HorizontalThreePane(None, width=320) as main:
+    with gui.Window(title="HorizontalThreePane", height=64, width=512) as window:
+        with forms.HorizontalThreePane(width=320, margin=(12, 12)) as main:
+            gui.ScrollField(text="Fixed left pane", ed=0, width=128)
+
             for index, (label, command) in enumerate(COMMANDS):
-                if index > 3:
+                if index > 2:
                     break
-                gui.Button(label=label).command += command
+                gui.Button(label=label, width=32).command += command
+            gui.ScrollField(text="Fixed right pane", ed=0, width=128)
 
     window.show()
 
@@ -175,10 +188,12 @@ def example_VerticalExpandForm(*args, **kwargs):
     import mGui.examples.formExamples as formExamples
     formExamples.example_VerticalExpandForm()
     """
-    with gui.Window(title="VerticalExpandForm") as window:
-        with forms.VerticalExpandForm(width=320) as main:
+    with gui.Window(title="VerticalExpandForm", height=128) as window:
+        with forms.VerticalExpandForm(width=320, margin=(12, 12)) as main:
+            gui.ScrollField(text="Components stack vertically", ed=0, height=24)
             for label, command in COMMANDS:
                 gui.Button(label=label).command += command
+            gui.ScrollField(text="last item stretches", ed=0, height=48)
 
     window.show()
 
@@ -190,7 +205,9 @@ def example_VerticalForm(*args, **kwargs):
     formExamples.example_VerticalForm()
     """
     with gui.Window(title="VerticalForm", resizeToFitChildren=True) as window:
-        with forms.VerticalForm(width=320) as main:
+        with forms.VerticalForm(width=320, margin=(12, 12)) as main:
+            gui.ScrollField(text="Components stack vertically", ed=0, height=24)
+
             for label, command in COMMANDS:
                 gui.Button(label=label).command += command
 
@@ -204,10 +221,10 @@ def example_VerticalStretchForm(*args, **kwargs):
     formExamples.example_VerticalStretchForm()
     """
     with gui.Window(title="VerticalStretchForm", height=400) as window:
-        with forms.VerticalStretchForm(width=320) as main:
+        with forms.VerticalStretchForm(width=320, margin=(12, 12)) as main:
+            gui.ScrollField(text="Components Stretch vertically", ed=0, height=24)
             for label, command in COMMANDS:
                 gui.Button(label=label).command += command
-
     window.show()
 
 
@@ -219,12 +236,14 @@ def example_VerticalThreePane(*args, **kwargs):
     formExamples.example_VerticalThreePane()
     """
     with gui.Window(title="VerticalThreePane") as window:
-        with forms.VerticalThreePane(None, width=320) as main:
+        with forms.VerticalThreePane(None, width=320, height=320, margin=(12, 12)) as main:
+            gui.ScrollField(text="Fixed Header, stretchy center", height=24, ed=False)
             for index, (label, command) in enumerate(COMMANDS):
                 if index > 3:
                     break
-                gui.Button(label=label).command += command
+                gui.Button(label=label, height=24).command += command
 
+            gui.ScrollField(text="Fixed footer", height=24, ed=0)
     window.show()
 
 
