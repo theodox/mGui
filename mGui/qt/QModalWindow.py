c6e101f36ac5af041b941a529165118abffdd0a3
bob-white <robert.wd.white@gmail.com>
2017-01-22 18:23:34 -0600

    Aliasing the rather verbose QtCore.Qt.WindowModality.ApplicationModal attribute.

--- mGui/qt/QModalWindow.py
+++ mGui/qt/QModalWindow.py
@@ -2,20 +2,19 @@
 Modal Window is a version of the Window class that blocks all input to other windows.
 However it does not block code execution, so it does not behave like a standard dialog.
 
-
 """
 from maya import cmds
 from mGui.core import BindingWindow
 from mGui.qt._compat import as_qt_object, QtCore
 
+APPLICATION_MODAL = QtCore.Qt.WindowModality.ApplicationModal
 
 class QModalWindow(BindingWindow):
 
     def __init__(self, *args, **kwargs):
         super(QModalWindow, self).__init__(*args, **kwargs)
         self._qt_obj = as_qt_object(self)
-        self._qt_obj.setWindowModality(
-            QtCore.Qt.WindowModality.ApplicationModal)
+        self._qt_obj.setWindowModality(APPLICATION_MODAL)
 
     def show(self):
         self._qt_obj.show()
