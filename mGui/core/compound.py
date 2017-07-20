fadf1dabd21d95b8229eddfd7db443878e9f57cd
bob-white <robert.wd.white@gmail.com>
2017-07-20 18:11:08 -0500

    Another pass on an IntSpinner CompoundControl
    Uses `__getattr__` and `__setattr__` to defer requests to the root
    control.
    Exposes the other controls as public fields.

--- mGui/core/compound.py
+++ mGui/core/compound.py
@@ -1,26 +1,27 @@
 
 import weakref
 
-from .controls import IntField, IconTextButton, Button
+from .controls import IntField, IconTextButton, Button, Text
 from ..forms import HorizontalStretchForm, VerticalStretchForm
 from ..events import MayaEvent
 
-class IntSpinner(IntField):
-    
+
+class IntSpinner(object):
+
     def __init__(self, key=None, **kwargs):
         self._step = kwargs.pop('step', 1)
+        value = kwargs.pop('value', 0)
+
         height = kwargs.pop('height', kwargs.pop('h', 32))
         width = kwargs.pop('width', kwargs.pop('w', 256))
-        with HorizontalStretchForm(key='IntSpinner#', height=height, width=width) as self._root:
-            super(IntSpinner, self).__init__(key, height=height, width=width - height, **kwargs)
-            with VerticalStretchForm():
-                self._up = Button(height=height / 2, width=height, label='^') #, style='iconOnly') #, image='caret-top')
-                self._down = Button(height=height / 2, width=height, label='v') #, style='iconOnly') #, image='caret-bottom')
+        with HorizontalStretchForm(key='IntSpinner#', **kwargs) as self.root:
+            self.field = IntField(height=height, width=width - height, value=value)
+            with VerticalStretchForm() as self.form:
+                self.increment = Button(height=height / 2, width=height, label='^')
+                self.decrement = Button(height=height / 2, width=height, label='v')
 
-        self._up.command += self._inc
-        self._down.command += self._dec
-        print(self.height, self.width)
-    
+        self.increment.command += self._inc
+        self.decrement.command += self._dec
 
     @property
     def step(self):
@@ -31,7 +32,18 @@ class IntSpinner(IntField):
         self._step = value
 
     def _inc(self, *args, **kwargs):
-        self.value += self._step
+        self.field.value += self._step
 
     def _dec(self, *args, **kwargs):
-        self.value -= self._step
+        self.field.value -= self._step
+
+    def __getattr__(self, attr):
+        if hasattr(self, 'root') and hasattr(self.root, attr):
+            return getattr(self.root, attr)
+        return super(IntSpinner, self).__getattribute__(attr)
+
+    def __setattr__(self, attr, value):
+        if hasattr(self, 'root') and hasattr(self.root, attr):
+            setattr(self.root, attr, value)
+        else:
+            self.__dict__[attr] = value
