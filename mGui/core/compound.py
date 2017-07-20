698b8d78c41850d3cd2a6374d721290f966c4196
bob-white <robert.wd.white@gmail.com>
2017-07-19 22:41:17 -0500

    Tweaked how some of the parameters are parsed.
    Also moved to using regular buttons with silly labels for now.
    Seems that `IconTextButton` has a minimum width that would cause it to
    overflow onto the `IntField`, and I'm not entirely sure what is causing
    it.

--- mGui/core/compound.py
+++ mGui/core/compound.py
@@ -1,7 +1,7 @@
 
 import weakref
 
-from .controls import IntField, IconTextButton
+from .controls import IntField, IconTextButton, Button
 from ..forms import HorizontalStretchForm, VerticalStretchForm
 from ..events import MayaEvent
 
@@ -9,14 +9,17 @@ class IntSpinner(IntField):
     
     def __init__(self, key=None, **kwargs):
         self._step = kwargs.pop('step', 1)
-        with HorizontalStretchForm(key='IntSpinner#') as self._root:
-            super(IntSpinner, self).__init__(key, **kwargs)
-            with VerticalStretchForm(width=24):
-                self._up = IconTextButton(height=12, style='iconOnly', image='caret-top')
-                self._down = IconTextButton(height=12, style='iconOnly', image='caret-bottom')
+        height = kwargs.pop('height', kwargs.pop('h', 32))
+        width = kwargs.pop('width', kwargs.pop('w', 256))
+        with HorizontalStretchForm(key='IntSpinner#', height=height, width=width) as self._root:
+            super(IntSpinner, self).__init__(key, height=height, width=width - height, **kwargs)
+            with VerticalStretchForm():
+                self._up = Button(height=height / 2, width=height, label='^') #, style='iconOnly') #, image='caret-top')
+                self._down = Button(height=height / 2, width=height, label='v') #, style='iconOnly') #, image='caret-bottom')
 
         self._up.command += self._inc
         self._down.command += self._dec
+        print(self.height, self.width)
     
 
     @property
